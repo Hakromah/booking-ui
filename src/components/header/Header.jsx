88ea906b6ec665e2b12f87f101a77df79c638a99
hassan <kkromah12@gmail.com>
2023-08-23 18:18:11 +0300

    new

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import './header.css';
 import { MdHotel, MdFlightTakeoff, MdAttractions } from 'react-icons/md';
 import { AiFillCar } from 'react-icons/ai';
-import { BsFillTaxiFrontFill } from 'react-icons/bs';
+import { BsFillTaxiFrontFill, BsFillPersonFill } from 'react-icons/bs';
+import { FaCalendarAlt } from 'react-icons/fa';
 
 const Header = () => {
 	return (
@@ -9,23 +10,23 @@ const Header = () => {
 			<div className="headerContainer">
 				<div className="headerList">
 					<div className="headerListItem active">
-						<MdHotel size={25}/>
+						<MdHotel size={25} />
 						<span>Stays</span>
 					</div>
 					<div className="headerListItem">
-						<MdFlightTakeoff size={25}/>
+						<MdFlightTakeoff size={25} />
 						<span>Flights</span>
 					</div>
 					<div className="headerListItem">
-						<AiFillCar size={20}/>
+						<AiFillCar size={20} />
 						<span>Car rentals</span>
 					</div>
 					<div className="headerListItem">
-						<MdAttractions size={30}/>
+						<MdAttractions size={30} />
 						<span>Arrractions</span>
 					</div>
 					<div className="headerListItem">
-						<BsFillTaxiFrontFill size={20}/>
+						<BsFillTaxiFrontFill size={20} />
 						<span>Airport taxis</span>
 					</div>
 				</div>
@@ -35,6 +36,27 @@ const Header = () => {
 					more with a free Hsk.Hotel account
 				</p>
 				<button className="headerButton">Sign in / Register</button>
+				{/* HEADER SEARCH AREA */}
+				<div className="headerSearch">
+					<div className="headerSearchItem">
+						<MdHotel size={25} className="headerIcon" />
+						<input
+							type="text"
+							placeholder="your destination...?"
+							className="headerSearchInput"
+						/>
+					</div>
+					<div className="headerSearchItem">
+						<FaCalendarAlt size={25} className="headerIcon" />
+						<span className="headerSearchText">date to date</span>
+					</div>
+					<div className="headerSearchItem">
+						<BsFillPersonFill size={25} className="headerIcon" />
+						<span className="headerSearchText">
+							2 adults 2 children 1 room
+						</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
